40bd1f80b8cbeacc8f69fa255f7b912c8f97f1fc
Yashdeep001 <112737611+Yashdeep001@users.noreply.github.com>
2023-11-09 16:12:21 -0700

    Lab3

--- App.js
+++ App.js
@@ -3,12 +3,21 @@
  *
  * @format
  */
-import React from 'react';
+import React, { useState } from 'react';
 import {View,} from 'react-native';
 import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
 
 export default function App() {
+
+    const [tasks,setTasks]=useState([
+        'Do laundry',
+        'Go to gym',
+        'Walk dog'
+    ])
+
+    
+    
     return (
         <View>
         <ToDoList />
--- ToDoList.js
+++ ToDoList.js
@@ -1,37 +1,36 @@
 import {  StyleSheet,Text, Pressable, SafeAreaView, ScrollView, View } from "react-native";
-
 import React from 'react';
-export default function ToDoList(){
+export default function ToDoList({tasks}){
     return(
         <SafeAreaView>
+           
             <ScrollView>
+            {tasks.map((tasks, index) => (
+           
                 <Pressable>
-                    <View style={[styles.task, styles.completed]}>
-                        <Text style={styles.taskText}>Do Laundry</Text>
-                    </View>
+                    <View key={tasks.index} style={[styles.task, styles.completed]}>
+                        <Text style={styles.taskText}>{tasks}</Text>
+                    </View> 
+           
                 </Pressable>
-                <Pressable>
-                    <View style={[styles.task]}>
-                        <Text style={styles.taskText}>Go to GYM</Text>
-                    </View>
-                </Pressable>
-                <Pressable>
-                    <View style={[styles.task, styles.completed]}>
-                        <Text style={styles.taskText}>Walk Dog</Text>
-                    </View>
-                </Pressable>
+                ))}
             </ScrollView>
+        
         </SafeAreaView>
-    )}
+    )
+}
 const styles = StyleSheet.create({
+    
     task: {
         padding: 10,
         borderBottomWidth: 1,
         borderColor: '#ccc',
     },
+    
     completed: {
         backgroundColor: '#e0e0e0',
     },
+    
     taskText: {
         fontSize: 16,
     },
